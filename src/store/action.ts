ca179b3fc135f69002bcdaf95521716a156e3c34
takeahot <takeahot@users.noreply.github.com>
2022-08-04 13:01:07 +0300

    delete type actions

--- src/store/action.ts
+++ src/store/action.ts
@@ -3,7 +3,7 @@ import { ActionType } from '../types/action'
 import { CategoriesType, SortTypesType } from '../types/app'
 import Answer from '../types/serverAnswer'
 
-export const changeKeyWord = createAction<string>(ActionType.ChangeKeyWord);
-export const changeCategory = createAction<CategoriesType>(ActionType.ChangeCategory); 
-export const changeSortBy = createAction<SortTypesType>(ActionType.ChangeSortBy); 
-export const loadBookList = createAction<Answer>(ActionType.LoadBookList); 
+export const changeKeyWord = createAction<string>('search/changeKeyWord');
+export const changeCategory = createAction<CategoriesType>('search/changeCategory'); 
+export const changeSortBy = createAction<SortTypesType>('search/changeSortBy'); 
+export const loadBookList = createAction<Answer>('data/loadBookList'); 
