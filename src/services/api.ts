d5fe49cbf81c813c4501abe24ef407cf273b46b7
takeahot <takeahot@users.noreply.github.com>
2022-08-09 10:04:49 +0300

    add error handle

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,36 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance , AxiosResponse, AxiosError } from 'axios';
+import { StatusCodes } from 'http-status-codes';
+import { processErrorHandle } from './process-error-handle';
 
 const BACKEND_URL = 'https://www.googleapis.com/books/v1';
 const REQUEST_TIMEOUT = 5000
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
+
+
 
 export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        processErrorHandle(error.response.data.error);
+      }
+
+      throw error;
+    }
+  );
 
   return api;
 };
